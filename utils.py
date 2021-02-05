b9b8318bd15f66db3312ceeb51d39dc1be7b8b9c
Rex Low <qiweilow950823@gmail.com>
2021-02-05 21:16:49 +0800

    feat: add nouns and verbs

--- utils.py
+++ utils.py
@@ -1,6 +1,13 @@
 import spacy
+from spacy.tokens.doc import Doc
 
-def ner_tagger(model: any=None, sentence:str="", mode: str="efficiency"):
+__all__ = [
+    "ner_tagger",
+    "get_nouns",
+    "get_verbs"
+]
+
+def get_spacy_model(model: any=None, mode: str="efficiency"):
 
     if model is None:
         if mode == "efficiency":
@@ -8,16 +15,41 @@ def ner_tagger(model: any=None, sentence:str="", mode: str="efficiency"):
         elif mode == "accuracy":
             model = spacy.load('en_core_web_trf')
         else:
-            raise Exception("Invalid mode for name tagger")
+            raise Exception("Invalid spacy mode")
+
+    return model
+
+def ner_tagger(doc: Doc):
+
+    if doc.__class__ is not Doc:
+        raise TypeError(f"Expecting spacy.tokens.doc.Doc but receive {doc.__class__}")
 
     tags = []
-    words = model(sentence)
-    for word in words:
-        [tags.append((word.text, ner.label_)) for ner in words.ents if (ner.text == word.text)]
-
+    for chunk in doc:
+        [tags.append((chunk.text, ner.label_)) for ner in doc.ents if (ner.text == chunk.text)]
+    
     return tags
+
+def get_nouns(doc: Doc):
+
+    if doc.__class__ is not Doc:
+        raise TypeError(f"Expecting spacy.tokens.doc.Doc but receive {doc.__class__}")
+
+    return [chunk.text for chunk in doc.noun_chunks]
+
+def get_verbs(doc: Doc):
+
+    if doc.__class__ is not Doc:
+        raise TypeError(f"Expecting spacy.tokens.doc.Doc but receive {doc.__class__}")
+
+    return [token.lemma_ for token in doc if token.pos_ == "VERB"]
 
 
 if __name__ == "__main__":
     sentence = "The DAP political education director said Khairy appears to be “much more” competent than Adham."
-    print(ner_tagger(sentence=sentence))
+    model = get_spacy_model()
+    docs = model(sentence)
+
+    print("NER: ", ner_tagger(docs))
+    print("Nouns: ", get_nouns(docs))
+    print("Verbs: ", get_verbs(docs))
