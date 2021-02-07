fa61532efd8fc618d414c57fe8cc3ad47b03b0cf
Rex Low <qiweilow950823@gmail.com>
2021-02-07 12:50:22 +0800

    feat: add an english tokenizer

--- utils.py
+++ utils.py
@@ -1,11 +1,15 @@
 import spacy
 from spacy.tokens.doc import Doc
+from spacy.lang.en import English
 
 __all__ = [
     "ner_tagger",
     "get_nouns",
-    "get_verbs"
+    "get_verbs",
+    "tokenize_english"
 ]
+
+spacy_english_parser = English()
 
 def get_spacy_model(model: any=None, mode: str="efficiency"):
 
@@ -44,12 +48,27 @@ def get_verbs(doc: Doc):
 
     return [token.lemma_ for token in doc if token.pos_ == "VERB"]
 
+def tokenize_english(text):
+    lda_tokens = []
+    tokens = spacy_english_parser(text=text)
+    for token in tokens:
+        if token.orth_.isspace():
+            continue
+        elif token.like_url:
+            lda_tokens.append("URL")
+        elif token.orth_.startswith("@"):
+            lda_tokens.append("SCREEN_NAME")
+        else:
+            lda_tokens.append(token.lower_)
+    return lda_tokens
+
 
 if __name__ == "__main__":
-    sentence = "The DAP political education director said Khairy appears to be “much more” competent than Adham."
+    sentence = "The DAP political education director said Khairy appears to be “much more” competent than Adham"
     model = get_spacy_model()
     docs = model(sentence)
 
     print("NER: ", ner_tagger(docs))
     print("Nouns: ", get_nouns(docs))
     print("Verbs: ", get_verbs(docs))
+    print("Tokenize english: ", tokenize_english(sentence))
